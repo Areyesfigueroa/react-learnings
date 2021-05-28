18fc09cbba96e9d9c4dc6f01d5185ebfdfe53a74
Aliel Reyes <alielreyes@gmail.com>
2021-05-27 21:57:09 -0700

    Split slices into their own files

--- 01-starting-project/src/components/Header.js
+++ 01-starting-project/src/components/Header.js
@@ -1,6 +1,6 @@
 import classes from "./Header.module.css";
 import { useSelector, useDispatch } from "react-redux";
-import { authActions } from "../store/index";
+import { authActions } from "../store/auth";
 import { useEffect } from "react";
 const Header = () => {
   const dispatch = useDispatch();
